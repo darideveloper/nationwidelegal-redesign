cd47dfa6509f29bba3d07afb221720a37feb2cd6
daridev <darideveloper@gmail.com>
2025-06-02 09:52:50 -0600

    feat: setup talwind and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import showTailwindcssBreakpoint from 'astro-show-tailwindcss-breakpoint';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react(), showTailwindcssBreakpoint()]
+});
